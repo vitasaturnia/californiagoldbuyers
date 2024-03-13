a415fda0543cc99da4fbf30afa042de0d0f6b066
vitasaturnia <53528807+vitasaturnia@users.noreply.github.com>
2024-03-13 12:45:12 -0700

    first

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,14 +3,16 @@ import uploadcare from 'netlify-cms-media-library-uploadcare'
 import cloudinary from 'netlify-cms-media-library-cloudinary'
 
 import AboutPagePreview from './preview-templates/AboutPagePreview'
-import BlogPostPreview from './preview-templates/BlogPostPreview'
-import ProductPagePreview from './preview-templates/ProductPagePreview'
+import ServicesPagePreview from './preview-templates/ServicesPagePreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
+import WholesalePagePreview from './preview-templates/WholesalePagePreview'
+import ProductsPagePreview from './preview-templates/ProductsPagePreview'
 
 CMS.registerMediaLibrary(uploadcare)
 CMS.registerMediaLibrary(cloudinary)
 
 CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
-CMS.registerPreviewTemplate('products', ProductPagePreview)
-CMS.registerPreviewTemplate('blog', BlogPostPreview)
+CMS.registerPreviewTemplate('services', ServicesPagePreview)
+CMS.registerPreviewTemplate('wholesale', WholesalePagePreview)
+CMS.registerPreviewTemplate('products', ProductsPagePreview)
